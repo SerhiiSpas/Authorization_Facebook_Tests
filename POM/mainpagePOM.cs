6adc661096209711d622389f2b15197ae9330e10
Serhii <43697311+SerhiiSpas@users.noreply.github.com>
2021-07-23 11:49:46 +0300

    Done ver1.2

---
+++ POM/mainpagePOM.cs
@@ -0,0 +1,86 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Authorization_Facebook_Tests.POM
+{
+    class mainpagePOM
+    {
+        private IWebDriver driver;
+
+        public mainpagePOM(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private readonly By _inputmail = By.XPath("//input[@type='text']");
+        private readonly By _inputpassword = By.XPath("//input[@type='password']");
+        private readonly By _clickentrance = By.XPath("//button[@name='login']");
+        private readonly By _clickiconbutton = By.XPath("//div[@aria-label='Обліковий запис']");
+        private readonly By _findname = By.XPath("//span[@class='d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j keod5gw0 nxhoafnm aigsh9s9 d3f4x2em fe6kdd0r mau55g9w c8b282yb mdeji52x a5q79mjw g1cxx5fr lrazzd5p oo9gr5id hzawbc8m']");
+
+
+        public mainpagePOM inputmail(string[] mailArray)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(x => driver.FindElement(_inputmail));
+            driver.FindElement(_inputmail).SendKeys(mailArray[0]);
+            return this;
+
+        }
+        public mainpagePOM inputmail2(string[] mailArray)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(x => driver.FindElement(_inputmail));
+            driver.FindElement(_inputmail).SendKeys(mailArray[1]);
+            return this;
+
+        }
+        public mainpagePOM inputmail3(string[] mailArray)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(x => driver.FindElement(_inputmail));
+            driver.FindElement(_inputmail).SendKeys(mailArray[2]);
+            return this;
+
+        }
+        public mainpagePOM inputnumber(string _number)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(x => driver.FindElement(_inputmail));
+            driver.FindElement(_inputmail).SendKeys(_number);
+            return this;
+
+        }
+        public mainpagePOM inputpassword(string _password)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(x => driver.FindElement(_inputpassword));
+            driver.FindElement(_inputpassword).SendKeys(_password);
+            return this;
+
+        }
+        public mainpagePOM clickentrance()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(x => driver.FindElement(_clickentrance));
+            driver.FindElement(_clickentrance).Click();
+            return this;
+
+        }
+        public mainpagePOM clickiconbutton()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(x => driver.FindElement(_clickiconbutton));
+            driver.FindElement(_clickiconbutton).Click();
+            return this;
+
+        }
+
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,4 @@
+using Authorization_Facebook_Tests.POM;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -9,83 +10,101 @@ namespace Authorization_Facebook_Tests
     
     public class Tests
     {
-        
         private IWebDriver driver;
 
-        private readonly By _inputmail = By.XPath("//input[@type='text']");
-        private readonly By _inputpassword = By.XPath("//input[@type='password']");
-        private readonly By _clickentrance = By.XPath("//button[@name='login']");
-        private readonly By _clickiconbutton = By.XPath("//div[@aria-label='Обліковий запис']");
         private readonly By _findname = By.XPath("//span[@class='d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j keod5gw0 nxhoafnm aigsh9s9 d3f4x2em fe6kdd0r mau55g9w c8b282yb mdeji52x a5q79mjw g1cxx5fr lrazzd5p oo9gr5id hzawbc8m']");
-
-
-        /* private readonly By _userlogin = By.XPath("//span[text()='Tomas Edison']");*/
-
-        //private const string _mail = "pikacas521@eyeremind.com";
         private const string _password = "1234567890Test";
         private const string _expectedLogin = "Tomas Edison";
-        private string[] mailArray = { "pikacas521@eyeremind.com", "pikacas5211@eyeremind.com" };
-        
+        private const string _number = "0669637279";
+        private string[] mailArray = { "pikacas521@eyeremind.com", "hobasa7621@activesniper.com", "hobasa@activesniper.com" };
         [SetUp]
         
         public void Setup()
         {
-            
-            
-            
-            
-            
 
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--disable-extensions"); // to disable extension
+            options.AddArguments("--disable-notifications"); // to disable notification
+            options.AddArguments("--disable-application-cache"); // to disable cache
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            driver.Manage().Window.Maximize(); //полноекраний режим
 
         }
 
         [Test]
         public void Test1()
         {
-            
-            for (int i = 0; i < mailArray.Length; i++)
-            {
-                ChromeOptions options = new ChromeOptions();
-                options.AddArguments("--disable-extensions"); // to disable extension
-                options.AddArguments("--disable-notifications"); // to disable notification
-                options.AddArguments("--disable-application-cache"); // to disable cache
-                driver = new ChromeDriver(options);
-                driver.Navigate().GoToUrl("https://www.facebook.com/");
-                driver.Manage().Window.Maximize(); //полноекраний режим
+            var mainpage = new mainpagePOM(driver);
 
-                Thread.Sleep(5000);
-                var signin1 = driver.FindElement(_inputmail);
-                signin1.SendKeys(mailArray[i]);
-                
+            mainpage.inputmail(mailArray);
+            mainpage.inputpassword(_password);
+            mainpage.clickentrance();
+            mainpage.clickiconbutton();
 
-                var signin2 = driver.FindElement(_inputpassword);
-                signin2.SendKeys(_password);
+            var actualLogin = driver.FindElement(_findname).Text;
 
-                var signin3 = driver.FindElement(_clickentrance);
-                signin3.Click();
+            Assert.AreEqual(_expectedLogin, actualLogin, "Test fail, login is wrong");
 
-                Thread.Sleep(5000);
-
-                var signin4 = driver.FindElement(_clickiconbutton);
-                signin4.Click();
-
-
-                Thread.Sleep(5000);
-
-                var actualLogin = driver.FindElement(_findname).Text;
-
-                Assert.AreEqual(_expectedLogin, actualLogin, "Test fail, login is wrong");
-
-                driver.Quit();
-                
-            }
 
         }
-        
+        [Test]
+        public void Test2()
+        {
+            var mainpage = new mainpagePOM(driver);
+
+            mainpage.inputmail2(mailArray);
+            mainpage.inputpassword(_password);
+            mainpage.clickentrance();
+            mainpage.clickiconbutton();
+
+
+            var actualLogin = driver.FindElement(_findname).Text;
+
+            Assert.AreEqual(_expectedLogin, actualLogin, "Test fail, login is wrong");
+
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var mainpage = new mainpagePOM(driver);
+
+            mainpage.inputnumber(_number);
+            mainpage.inputpassword(_password);
+            mainpage.clickentrance();
+            mainpage.clickiconbutton();
+
+
+            var actualLogin = driver.FindElement(_findname).Text;
+
+            Assert.AreEqual(_expectedLogin, actualLogin, "Test fail, login is wrong");
+
+        }
+        [Test]
+        public void Test4_negative()
+        {
+            var mainpage = new mainpagePOM(driver);
+
+            mainpage.inputmail3(mailArray);
+            mainpage.inputpassword(_password);
+            mainpage.clickentrance();
+            mainpage.clickiconbutton();
+
+
+            var actualLogin = driver.FindElement(_findname).Text;
+
+            Assert.AreEqual(_expectedLogin, actualLogin, "Test fail, login is wrong");
+            
+        }
         [TearDown]
         public void TearDown()
         {
-            
+            driver.Quit();
+            if (driver != null)
+            {
+                driver.Quit();
+            }
         }
     }
 }
